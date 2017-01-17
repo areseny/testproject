b1f92a2348c505ac23ffe643e5a4549e515dd8f7
charlie-ablett <im.charlie.ablett@gmail.com>
2017-01-17 20:53:35 +1300

    slug issue

--- app/logic/slug_methods.rb
+++ app/logic/slug_methods.rb
@@ -9,12 +9,17 @@ module SlugMethods
 
   def generate_unique_slug
     @all_chain_slugs ||= ProcessChain.all.map(&:slug)
+    ap "All slugs:"
+    ap @all_chain_slugs.uniq
     return if slug.present?
+    ap "Slug not present"
     while @new_slug.nil? || slug_not_unique?
+      ap "Looping: new slug nil? #{@new_slug.nil?}"
+      ap "Looping: slug not unique? #{slug_not_unique?}"
       @new_slug = generate_slug
       ap "Slug for #{self.id} generated: `#{@new_slug}`"
-      ap "Slug already exists? #{slug_not_unique?}"
     end
+    ap "out of loop"
     self.slug = @new_slug
   end
 
