4ff9c7a01afc1da6e4d9e5ccb85e6bc1e3edc9b6
charlie-ablett <im.charlie.ablett@gmail.com>
2017-05-03 21:54:07 +1200

    Refactor serialisers, deep nesting by default.

--- app/serializers/process_chain_serializer.rb
+++ app/serializers/process_chain_serializer.rb
@@ -7,10 +7,6 @@ class ProcessChainSerializer < ActiveModel::Serializer
 
   attributes :id, :recipe_id, :executed_at, :finished_at, :process_steps, :created_at, :input_file_manifest, :output_file_manifest
 
-  # def process_steps
-  #   object.process_steps.sort_by{ |step| step.position }.map{|step| ActiveModelSerializers::SerializableResource.new(step, adapter: :attribute).as_json}
-  # end
-
   def executed_at
     object.executed_at.nil? ? nil : object.executed_at.iso8601
   end
