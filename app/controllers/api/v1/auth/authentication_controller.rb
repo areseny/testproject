3003e9d785f3315bdd0115850c775ee90c1b1693
charlie-ablett <im.charlie.ablett@gmail.com>
2017-05-05 22:17:18 +1200

    move sign in to auth

--- app/controllers/api/v1/auth/authentication_controller.rb
+++ app/controllers/api/v1/auth/authentication_controller.rb
@@ -1,28 +1,21 @@
 module Api::V1::Auth
   class AuthenticationController < ApplicationController
     def sign_in
-      @account = Account.find_for_database_authentication(email: params[:email])
-      if @account.present? && @account.valid_password?(params[:password])
-        set_response_headers
-        render json: {data: account_payload}
+      account = Account.find_for_database_authentication(email: params[:email])
+      if account.valid_password?(params[:password])
+        render json: account_payload(account)
       else
-        render json: {errors: ["Invalid login credentials. Please try again."]}, status: :unauthorized
+        render json: {errors: ['Invalid credentials']}, status: :unauthorized
       end
     end
 
     private
 
-    def set_response_headers
-      response.headers["Access-Token"] = @account.generate_token
-      response.headers["Token-Type"] = "Bearer"
-      response.headers["Client"] = SecureRandom.urlsafe_base64(nil, false)
-      response.headers["uid"] = @account.uid
-    end
-
-    def account_payload
-      return nil unless @account and @account.id
+    def account_payload(account)
+      return nil unless account and account.id
       {
-          account: {id: @account.id, email: @account.email, admin: @account.admin?, uid: @account.uid, name: @account.name}
+          auth_token: JsonWebToken.encode({account_id: account.id}),
+          account: {id: account.id, email: account.email, admin: account.admin?}
       }
     end
 
