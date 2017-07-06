65cbde15ab30e9b0add670d3f92fec2850e02e52
charlie-ablett <im.charlie.ablett@gmail.com>
2017-07-06 21:55:47 +1200

    Step class collector dynamically loads step classe from gems

--- app/logic/step_class_collector.rb
+++ app/logic/step_class_collector.rb
@@ -1,44 +1,81 @@
 class StepClassCollector
 
-  def self.step_class_hash
-    step_classes.map{|klass| { name: klass.name, description: klass.description } }
+  class << self
+    def step_class_hash
+      step_classes.map{|klass| { name: klass.name, description: klass.description } }
+    end
+
+    def step_gem_hash
+      step_gems.each do |gem_info_hash|
+        gem_info_hash[:step_classes] = gem_info_hash[:step_classes].map{|klass| { name: klass.name, description: klass.description } }
+      end
+    end
+
+    def step_classes
+      klasses = step_gems.map{|gem_hash| gem_hash[:step_classes]}.flatten
+      [InkStep::Base] + klasses.sort_by(&:to_s)
+    end
   end
 
-  def self.step_classes
-    # get all directories under gems that match ink_step
-    # grab all the class names under those directories
-    # minus Engine, minus Base
+  private
 
-    # Dir.glob('/path/to/namespaced/directory/*').collect{|file_path| File.basename(file_path, '.rb').constantize}
+  class << self
+    def step_gems
+      # Find any gem starting with `inkstep_` using Bundler's gem specs
+      ink_step_gem_specs = Bundler.load.specs.select{|gem| gem.name.match /^inkstep_/}
 
-    # require 'pathname'
+      # structure:
+      # { name: "inkstep_example_step_gem", version: 1.1, git_version: "abcdef", step_classes: [InkStep::AStep, InkStep::AnotherStep], source: "https://gitlab.coko.foundation/inkstep_example_step_gem" }
+      step_gems = []
+      ink_step_gem_specs.each do |gem_spec|
+        gem_hash = {}
+        gem_hash[:name] = gem_spec.name
+        gem_hash[:version] = gem_spec.version
+        gem_hash[:git_version] = gem_spec.git_version
+        gem_hash[:repo] = get_repo(gem_spec)
+        gem_hash[:step_classes] = load_step_classes(gem_spec)
+        step_gems << gem_hash
+      end
+      step_gems
+    end
 
-    # make this the directory you are autoloading from
-    # autoload_dir = File.join(Rails.application.root, 'app', 'models')
+    def get_repo(source)
+      if source.is_a?(Bundler::Source::Git)
+        return source.uri
+      elsif source.is_a?(Bundler::Source::Path)
+        return source.original_path
+      end
+      source.to_s
+    end
 
-    # this will return FooBar::BarFoo::**::*Boo constants and autoload them
-    # Dir.glob(File.join(autoload_dir, 'foo_bar', 'bar_foo', '**', '*_boo.rb')).collect{|pathname| Pathname.new(pathname.chomp('.rb')).relative_path_from(Pathname.new(autoload_dir)).to_s.camelize.constantize}
+    def load_step_classes(gem_spec)
+      # e.g. /home/charlie/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0/bundler/gems/inkstep_coko_demo_steps-7eafb06c791d/
+      gem_path = gem_spec.full_gem_path
 
-    # OR could do?
-    # Dir["app/models/foo/*.rb"].each {|file| load file}
-
-    classes = [
-        InkStep::ShoutifierStep,
-        InkStep::RotThirteenStep,
-        InkStep::CalibreHtmlToEpubStep,
-        InkStep::VivliostyleHtmlToPdfStep,
-        InkStep::PandocEpubToIcmlStep,
-        InkStep::WkHtmlToPdfStep,
-        InkStep::PandocDocxToHtmlStep,
-        InkStep::XsweetPipeline::DocxExtract::DocxToHtmlExtractStep,
-        InkStep::XsweetPipeline::DocxExtract::HandleNotesStep,
-        InkStep::XsweetPipeline::DocxExtract::JoinElementsStep,
-        InkStep::XsweetPipeline::DocxExtract::ScrubStep,
-        InkStep::XsweetPipeline::DocxExtract::CollapseParagraphsStep,
-        InkStep::XsweetPipeline::HeaderPromote::HeaderPromotionStep,
-        InkStep::XsweetPipeline::FinaliseTypescript::FinalRinseStep,
-        InkStep::XsweetPipeline::PrepareForEditoria::EditoriaPrepareStep
-    ].uniq
+      # Dive into it and grab anything in the `lib/#{gemname minus inkstep_}/ink_step` that is an extension of Base or something else that is an extension of Base
+      inner_module_name = gem_spec.name.gsub("inkstep_", "")
+      step_directory = File.join(gem_path, "lib", inner_module_name)
+      step_classes = []
+      begin
+        Dir.chdir(File.join(step_directory, "ink_step")) do
+          Dir.glob(File.join(step_directory, "**", "*_step.rb")).collect do |pathname|
+            class_name = Pathname.new(pathname.chomp('.rb')).relative_path_from(Pathname.new(step_directory)).to_s.camelize
+            begin
+              klass = class_name.constantize
+              step_classes << klass if klass.ancestors.include? InkStep::Base
+            rescue => e
+              ap "Couldn't load the class #{class_name} in gem #{gem_spec.name} located in #{pathname}"
+              ap "Message: #{e.message}"
+            end
+          end
+        end
+      rescue => e
+        ap "There's a problem getting the files from the directory #{File.join(step_directory, "ink_step")}... Try running `bundle install`"
+        ap "message: #{e.message}"
+        return []
+      end
+      step_classes
+    end
   end
 
 end
